6f525867824a8386b4a1bdec1be0341bbbe5595f
nastu <nastushik92@bk.ru>
2022-08-24 18:45:51 +0300

    третий комит

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,25 @@ public class Main {
         } else if (clientOS==1){
             System.out.println("Установите версию приложения для Android по ссылке");
         }
+        //Задание 2
+
+        int clientDeviceYear = 2014;
+         if (clientOS == 0 &&clientDeviceYear<2015 ){
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS ==1 && clientDeviceYear < 2015){
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }else if(clientOS == 0 && clientDeviceYear>2015){
+             System.out.println("Установите версию приложения для iOS по ссылке");
+         } else if (clientOS==1 && clientDeviceYear> 2015){
+             System.out.println("Установите версию приложения для Android по ссылке");
+         }
     }
+    //Задание 3
+    int year = 2021;
+
 }
+
+
+
+
+
